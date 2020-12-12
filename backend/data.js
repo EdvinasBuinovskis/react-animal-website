f874e573a0305ae59a935a5f5646fd53feded1a6
unknown <edvinas.buinovskis@stud.vgtu.lt>
2020-12-12 15:23:33 +0200

    Setting up express server

---
+++ backend/data.js
@@ -0,0 +1,75 @@
+const data = {
+    animals: [
+        {
+            _id: '1',
+            name: 'John',
+            category: 'Dog',
+            image: '/images/a1.jpg',
+            status: 'Atiduodamas',
+            date: '2020-11-11',
+            description: 'Nice dog',
+            telNum: '123456'
+        },
+        {
+            _id: '2',
+            name: 'Petras',
+            category: 'Cat',
+            image: '/images/a2.jpg',
+            status: 'Atiduodamas',
+            date: '2020-11-11',
+            description: 'Nice cat',
+            telNum: '123456'
+        },
+        {
+            _id: '3',
+            name: 'Mr Dog',
+            category: 'Dog',
+            image: '/images/a3.jpg',
+            status: 'Ieskomas',
+            date: '2020-11-10',
+            description: 'Nice dog',
+            telNum: '123456'
+        },
+        {
+            _id: '4',
+            name: 'Jonas',
+            category: 'Cat',
+            image: '/images/a4.jpg',
+            status: 'Atiduodamas',
+            date: '2020-10-11',
+            description: 'Nice cat',
+            telNum: '123456'
+        },
+        {
+            _id: '5',
+            name: 'John',
+            category: 'Dog',
+            image: '/images/a5.jpg',
+            status: 'Atiduodamas',
+            date: '2020-11-11',
+            description: 'Nice dog',
+            telNum: '123456'
+        },
+        {
+            _id: '6',
+            name: 'John',
+            category: 'Dog',
+            image: '/images/a6.jpg',
+            status: 'Atiduodamas',
+            date: '2020-11-11',
+            description: 'Nice dog',
+            telNum: '123456'
+        },
+        {
+            _id: '7',
+            name: 'John',
+            category: 'Dog',
+            image: '/images/a7.jpg',
+            status: 'Atiduodamas',
+            date: '2020-11-11',
+            description: 'Nice dog',
+            telNum: '123456'
+        },
+    ], 
+};
+export default data;
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+app.get('/api/animals', (req, res) => {
+    res.send(data.animals);
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () =>{
+    console.log(`Serve at http://localhost:${port}`);
+});
