829cbaacdceba4b473eb4f5af5bd991f0580e3df
unknown <edvinas.buinovskis@stud.vgtu.lt>
2020-12-12 16:00:38 +0200

    loading animals from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import Animal from '../components/Animal';
-import data from '../data';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
+    const [animals, setAnimals] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    useEffect(() =>{
+        const fetchData = async () =>{
+            try{
+                setLoading(true);
+                const { data } = await axios.get('/api/animals');
+                setLoading(false);
+                setAnimals(data);
+            }catch (err) {
+                setError(err.message);
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
     return (
         <div>
-            <div className="row center">
-                {data.animals.map(animal => (
-                    <Animal key={animal._id} animal={animal}></Animal>
-                ))}
-            </div>
+            {loading? (<LoadingBox></LoadingBox>) :
+                error? (<MessageBox variant="danger">{error}</MessageBox>) :
+                (
+                <div className="row center">
+                    {animals.map(animal => (
+                        <Animal key={animal._id} animal={animal}></Animal>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
